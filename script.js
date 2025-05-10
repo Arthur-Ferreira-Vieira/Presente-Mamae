739eb3da3f20f0a2ad15cb6fad92e3b9c92f9337
Arthur-Ferreira-Vieira <arthurferreiravieira12@gmail.com>
2025-05-10 18:46:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Carrossel automático
+const imagens = [
+    'foto1.jpg', 'foto2.jpeg', 'foto3.jpeg', 'foto4.jpeg', 'foto5.jpeg',
+    'foto6.jpeg', 'foto7.jpeg', 'foto8.jpeg', 'foto9.jpeg'
+  ];
+  
+  let indice = 0;
+  const imgElement = document.getElementById('carrossel-img');
+  
+  function trocarImagem() {
+    indice = (indice + 1) % imagens.length;
+    imgElement.src = `img/${imagens[indice]}`;
+  }
+  
+  setInterval(trocarImagem, 3000); // troca a cada 3 segundos
+  
+  // Mostrar carta
+  function mostrarCarta() {
+    document.getElementById('carta').style.display = 'block';
+  }
+  
